6222b7e3ba5005ddcf431e5c112737156e2c3530
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-10-22 16:26:14 -0700

    Feature: added login route

--- src/api/users.py
+++ src/api/users.py
@@ -18,11 +18,8 @@ class User(BaseModel):
 def register(user: User):
     try:
         with db.engine.begin() as con:
-            add_user_query = sqlalchemy.text('INSERT INTO users (email) VALUES (:email) RETURNING id')
-            user_id = con.execute(add_user_query, user.email).fetchone().id
-
-            set_username_query = sqlalchemy.text('INSERT INTO username_history (user_id, username) VALUES (:user_id, :username)')
-            con.execute(set_username_query, {'user_id': user_id, 'username': user.username})
+            add_user_query = sqlalchemy.text('INSERT INTO users (email, username) VALUES (:email, :username) RETURNING id')
+            user_id = con.execute(add_user_query, {'email': user.email,  'username': user.username}).fetchone()
     except Exception as e:
         print(e)
         return {'success': False, 'error': str(e)}
@@ -30,15 +27,28 @@ def register(user: User):
     return {'success': True}
 
 
+@router.post("/login")
+def login(user: User):
+    try:
+        with db.engine.begin() as con:
+            user_exists_query = sqlalchemy.text('SELECT id FROM users WHERE email = :email AND username = :username')
+            user = con.execute(user_exists_query, {'email': user.email, 'username': user.username}).fetchone()
+
+            if user is None:
+                raise Exception("User with given username and email does not exist")
+    except Exception as e:
+        print(e)
+        return {'success': False, 'error': str(e)}
+
+    return {'success': True, 'user_id': user.id}
+
+
 @router.put("/{user_id}/username")
 def update_username(user_id: int, username: str):
     try:
         with db.engine.begin() as con:
-            set_username_query = sqlalchemy.text("""
-                INSERT INTO username_history (user_id, username)
-                VALUES (:user_id, :username)
-            """)
-            con.execute(set_username_query, {'email': user_id, 'username': username})
+            set_username_query = sqlalchemy.text('UPDATE users SET username = :username WHERE id = :user_id')
+            con.execute(set_username_query, {'username': username, 'user_id': user_id})
     except Exception as e:
         print(e)
         return {'success': False, 'error': str(e)}
