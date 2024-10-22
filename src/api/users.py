85d4cac62eb94c6d01859b7428c825e696995b62
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-10-22 15:47:03 -0700

    update: reformatted users.py, added packages to requirements.txt

--- src/api/users.py
+++ src/api/users.py
@@ -1,35 +1,15 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
-from src.api import auth
 import sqlalchemy
 from src import database as db
 
 router = APIRouter(
-    prefix="/info",
-    tags=["info"],
-    dependencies=[Depends(auth.get_api_key)],
+    prefix="/users",
+    tags=["users"],
 )
 
 
-class Timestamp(BaseModel):
-    day: str
-    hour: int
-
-
-current_timestamp = None
-
-
-@router.post("/current_time")
-def post_time(timestamp: Timestamp):
-
-    with db.engine.begin() as connection:
-        time_query = sqlalchemy.text(
-            'INSERT INTO times (day, hour) VALUES (:day, :hour)'
-        )
-        connection.execute(time_query, {'day': timestamp.day, 'hour': timestamp.hour})
-
-    global current_timestamp
-    current_timestamp = timestamp
-
-    return "OK"
+@router.post("/create_user")
+def create_user():
+    pass
 
