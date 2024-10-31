045922e27303ece46adc218890b4bf4c023f645f
Ian Ang <iannangg02@gmail.com>
2024-10-30 18:47:38 -0700

    update: implemented SQL queries into variables to be executed by the db connection

--- src/api/leaderboards.py
+++ src/api/leaderboards.py
@@ -16,25 +16,24 @@ def get_entrants_leaderboard(game_id):
 
     """
 
-    """
-
-    SELECT 
-        entrants.name AS entrant_name, 
-        entrants.weapon AS entrant_weapon, 
-        COUNT(match_victors.entrant_id) AS total_wins,
-        DENSE_RANK() OVER (ORDER BY COUNT(match_victors.entrant_id) DESC) AS rank
-    FROM entrants
-    JOIN match_victors ON match_victors.entrant_id = entrants.id
-    WHERE entrants.game_id = :game_id
-    GROUP BY entrants.game_id, entrants.name, entrants.weapon
-    ORDER BY rank, total_wins DESC
-    LIMIT 10
-
-    """
     print("game_id = ", game_id)
 
+    sql_to_execute = """
+                        SELECT 
+                            entrants.name AS entrant_name, 
+                            entrants.weapon AS entrant_weapon, 
+                            COUNT(match_victors.entrant_id) AS total_wins,
+                            DENSE_RANK() OVER (ORDER BY COUNT(match_victors.entrant_id) DESC) AS rank
+                        FROM entrants
+                        JOIN match_victors ON match_victors.entrant_id = entrants.id
+                        WHERE entrants.game_id = :game_id
+                        GROUP BY entrants.game_id, entrants.name, entrants.weapon
+                        ORDER BY rank, total_wins DESC
+                        LIMIT 10
+                    """
+    
     with db.engine().begin() as connection:
-        entrants_leaderboard = connection.execute(sqlalchemy.text(""), {"game_id" : game_id}).mappings.fetchall()
+        entrants_leaderboard = connection.execute(sqlalchemy.text(sql_to_execute), {"game_id" : game_id}).mappings.fetchall()
 
         print("entrants_leaderboard = ", entrants_leaderboard)
 
@@ -59,27 +58,25 @@ def get_users_leaderboard(game_id):
 
     """
 
-    """
-    
-    SELECT 
-        username, 
-        SUM(balance_change) AS total_earnings,
-        DENSE_RANK() OVER (ORDER BY SUM(balance_change) DESC) AS rank
-    FROM profiles
-    JOIN user_balances ON user_balances.user_id = profiles.user_id
-    JOIN matches ON matches.id = user_balances.match_id
-    JOIN rounds ON rounds.id = matches.round_id
-    WHERE rounds.game_id = :game_id
-    GROUP BY username
-    ORDER BY rank, total_earnings DESC
-    LIMIT 10
-    
-    """
-
     print("game_id = ", game_id)
 
+    sql_to_execute = """
+                        SELECT 
+                            username, 
+                            SUM(balance_change) AS total_earnings,
+                            DENSE_RANK() OVER (ORDER BY SUM(balance_change) DESC) AS rank
+                        FROM profiles
+                        JOIN user_balances ON user_balances.user_id = profiles.user_id
+                        JOIN matches ON matches.id = user_balances.match_id
+                        JOIN rounds ON rounds.id = matches.round_id
+                        WHERE rounds.game_id = :game_id
+                        GROUP BY username
+                        ORDER BY rank, total_earnings DESC
+                        LIMIT 10
+                    """
+    
     with db.engine().begin() as connection:
-        users_leaderboard = connection.execute(sqlalchemy.text(""), {"game_id" : game_id}).mappings.fetchall()
+        users_leaderboard = connection.execute(sqlalchemy.text(sql_to_execute), {"game_id" : game_id}).mappings.fetchall()
 
         print("users_leaderboard = ", users_leaderboard)
 
