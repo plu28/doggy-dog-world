f14cbca49352400d09d6275dfcb808eed5183c21
David Voronov <davidvoronof@gmail.com>
2024-11-02 11:18:44 -0700

    add: get user balance given a uuid and a game_id

--- src/api/gameplay.py
+++ src/api/gameplay.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 import sqlalchemy
 from src import database as db
+import re
 
 router = APIRouter(
     prefix="/gameplay",
@@ -111,6 +112,45 @@ def get_active_match_entrants(match_id: int):
         'entrant1_id': match_dict['entrant_one'],
         'entrant2_id': match_dict['entrant_two']
     }
+
+# NOTE: Ugly error if you pass in a string that is not a uuid format
+@router.get("/balance/{uuid}/{game_id}")
+def get_balance(uuid: str, game_id: int):
+    uuid_pattern = r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$" # chatgpt regex pattern because regex was not made for humans
+    try:
+        if not re.match(uuid_pattern, uuid):
+            raise Exception("Invalid uuid")
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
+    try:
+        with db.engine.begin() as con:
+            select_query = sqlalchemy.text('''
+                WITH game_matches AS (
+                    SELECT matches.id AS matches
+                    FROM matches
+                    JOIN rounds ON rounds.id = matches.round_id
+                    JOIN games ON games.id = rounds.game_id
+                    WHERE games.id = :game_id
+                )
+                SELECT SUM(user_balances.balance_change) AS balance
+                FROM
+                    user_balances
+                WHERE match_id IN (SELECT game_matches.matches FROM game_matches)
+                AND user_id = :uuid
+                ''')
+            user_balance = con.execute(select_query, {
+                'game_id': game_id,
+                'uuid': uuid
+            }).scalar_one_or_none()
+        if user_balance == None:
+            raise Exception(f"Balance not found for {uuid} for game_id: {game_id}")
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
+    return {'balance': user_balance}
 
 
 
