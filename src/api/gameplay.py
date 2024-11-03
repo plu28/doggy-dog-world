aacca682162f5cf13f70c8122eab4f49facaf2c9
David Voronov <davidvoronof@gmail.com>
2024-11-02 18:02:32 -0700

    add: betting

--- src/api/gameplay.py
+++ src/api/gameplay.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Request
+from sqlalchemy.exc import IntegrityError
 from pydantic import BaseModel
 import sqlalchemy
 from src import database as db
@@ -9,8 +10,6 @@ router = APIRouter(
     tags=["gameplay"],
 )
 
-# TODO: Look into returning HTTP status codes if an error occurs.
-
 # GET: active game id
 @router.get("/get_games")
 def active_game():
@@ -20,9 +19,9 @@ def active_game():
                 SELECT id
                 FROM games
                 WHERE NOT EXISTS (
-                  SELECT 1
-                  FROM completed_games
-                  WHERE game_id = games.id
+                    SELECT 1
+                    FROM completed_games
+                    WHERE game_id = games.id
                 )
                 ''')
             game_id = con.execute(select_query).scalar_one_or_none()
@@ -116,6 +115,7 @@ def get_active_match_entrants(match_id: int):
 # GET: Returns a user balance for a given user and a game in which they had balance change
 @router.get("/balance/{uuid}/{game_id}")
 def get_balance(uuid: str, game_id: int):
+    # Make sure uuid is of a valid form (query throws a nasty error otherwise)
     uuid_pattern = r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$" # chatgpt regex pattern because regex was not made for humans
     try:
         if not re.match(uuid_pattern, uuid):
@@ -152,11 +152,167 @@ def get_balance(uuid: str, game_id: int):
 
     return {'balance': user_balance}
 
+class Bet(BaseModel):
+    match_id: int
+    entrant_id: int
+    bet_amount: int
+
+# Insert into bets and user_balances
+# Ensure bet amount is less than or equal to user balance
+# Ensure bet is being placed on an active match
+# Ensure bet is being placed on an entrant in that match
+# NOTE: This current implementation would technically enable a user to place bets an unlimited amount of times.
+#       this could be an issue in the theoretical case where a bad actor wants to run a script to just insert a
+#       bajillion rows into our db.
+#       A potential fix would be to limit one bet, per user, per entrant
+@router.post("/bet/{uuid}")
+def place_bet(uuid: str, bet: Bet):
+    # Make sure uuid is of a valid form (query throws a nasty error otherwise)
+    uuid_pattern = r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$" # chatgpt regex pattern because regex was not made for humans
+    try:
+        if not re.match(uuid_pattern, uuid):
+            raise Exception("Invalid uuid")
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
+    if bet.bet_amount == 0:
+        return {'error': 'You can not bet 0'}
+
+    try:
+        with db.engine.begin() as con:
+            # NOTE: This transaction is vulnerable to concurrency issues that I don't know how to fix rn
+            # NOTE: This query is likely larger than it has to be and should be consolidated at some point
+            # NOTE: A lot of this query should probably be a view lmao
+            cte = '''
+            -- Makes round_id and game_id available to the rest of the query.
+            -- Table is empty if game_id, round_id, or match_id are not active
+            WITH match_round AS (
+                SELECT
+                    rounds.game_id AS game_id,
+                    rounds.id AS round_id
+                FROM
+                    rounds
+                JOIN matches ON matches.round_id = rounds.id
+                -- Ensure the game, round, and match are active
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM completed_matches
+                    WHERE completed_matches.id = :match_id
+                )
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM completed_rounds
+                    WHERE completed_rounds.round_id = rounds.id
+                )
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM completed_games
+                    WHERE completed_games.game_id = rounds.game_id
+                )
+            ),
+            -- Gets the matches relevant to the users balance (dependency)
+            game_matches AS (
+                SELECT matches.id AS matches
+                FROM matches
+                JOIN rounds ON rounds.id = matches.round_id
+                JOIN games ON games.id = rounds.game_id
+                WHERE games.id IN (SELECT game_id FROM match_round)
+            ),
+            -- Gets the balance of the user placing the bet
+            user_balance AS (
+                SELECT SUM(user_balances.balance_change) AS balance
+                FROM
+                    user_balances
+                WHERE match_id IN (SELECT game_matches.matches FROM game_matches)
+                AND user_id = :uuid
+            ),
+            -- Gets the amount the user has already bet
+            current_user_bets AS (
+                SELECT SUM(bets.amount) AS current_bet_amount
+                FROM bets
+                WHERE match_id = :match_id
+            )
+            '''
+
+            conditions = '''
+            -- Ensure the user is not betting an amount greater than their balance
+            WHERE NOT EXISTS (
+                SELECT balance
+                FROM user_balance
+                WHERE user_balance.balance < :amount
+            )
+            -- Ensure the game/round/match are active
+            AND EXISTS (
+                SELECT 1
+                FROM match_round
+            )
+            -- Ensure entrant is in this match
+            AND EXISTS (
+                SELECT
+                    matches.entrant_one,
+                    matches.entrant_two
+                FROM
+                    matches
+                WHERE matches.entrant_one = :entrant_id
+                OR matches.entrant_two = :entrant_id
+                AND matches.id = :match_id
+            )
+            '''
+
+            insert_into_bets_query = sqlalchemy.text(f'''{cte}
+                INSERT INTO bets (user_id, entrant_id, match_id, amount)
+                SELECT :uuid, :entrant_id, :match_id, :amount
+                {conditions}
+                -- This condition can not be shared between the two.
+                    -- This query inserts into bets, and this condition checks for the state of bets.
+                    -- This means the state of bets will not be the same between the two queries and the condition will not be the same
+                -- Ensure that user is not betting an amount less than they've already bet
+                AND NOT EXISTS (
+                    SELECT current_bet_amount
+                    FROM current_user_bets
+                    WHERE current_bet_amount < -(:amount)
+                )
+            ''')
+
+            insert_into_user_balances_query = sqlalchemy.text(f'''{cte}
+                INSERT INTO user_balances (user_id, balance_change, match_id)
+                SELECT :uuid, -(:amount), :match_id
+                {conditions}
+            ''')
+
+            insert_into_bets_status = con.execute(insert_into_bets_query, {
+                'uuid': uuid,
+                'entrant_id': bet.entrant_id,
+                'match_id': bet.match_id,
+                'amount': bet.bet_amount
+            }).rowcount
+            if insert_into_bets_status > 1:
+                raise Exception("Strange error occured. Placing bet somehow inserted more than 1 row")
+            elif insert_into_bets_status < 1:
+                raise Exception("Bet placement failed. Can you afford this bet? Did you bet on an entrant in an active match?")
+
+            insert_into_user_balances_status = con.execute(insert_into_user_balances_query, {
+                'uuid': uuid,
+                'entrant_id': bet.entrant_id,
+                'match_id': bet.match_id,
+                'amount': bet.bet_amount
+            }).rowcount
+            print(insert_into_user_balances_status)
+            if insert_into_user_balances_status != 1:
+                raise Exception("A concurrency error likely occurred") from None
+
+    except IntegrityError as e:
+        return {'error': "Attempted to bet on a match that hasn't even been created yet"}
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
+    return "OK"
 
 
 
-# GET: retrieve user balance
-# POST: place a bet on an entrant
+
 # GET: retrieve post story
 # GET: retrieve winner
 # GET: how much they gained
