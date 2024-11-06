99939ac6beb1be24fae13fc59a441273ab8a856f
David Voronov <122059007+plu28@users.noreply.github.com>
2024-11-06 11:08:46 -0800

    Fixed both betting and user_balances endpoint to retrieve balances. (#15)
    * update: fixed betting
    
    * update: catch duplicate username error message and include psycopg2 in requirements.txt
    
    * update: made betting/get balance endpoints not require a uuid and instead go off the current logged in user
    
    * fix: can now add more than 1 entrant to a game
    
    * fix: fixed bets not going through

--- src/api/users.py
+++ src/api/users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request, Response
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel, EmailStr
+from psycopg2.errors import UniqueViolation
 from typing import Optional
 import sqlalchemy
 from src import database as db
