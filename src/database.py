a7e04b5edfa1d1da5688156fe341c2048d790ac7
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-29 19:09:27 -0800

    Added: def to get conn url with async driver

--- src/database.py
+++ src/database.py
@@ -10,6 +10,12 @@ def database_connection_url():
     return os.environ.get("POSTGRES_URI")
 
 
+def database_async_connection_url():
+    dotenv.load_dotenv()
+
+    return os.environ.get("ASYNC_POSTGRES_URI")
+
+
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
-async_engine = create_async_engine(database_connection_url(), pool_pre_ping=True)
+async_engine = create_async_engine(database_async_connection_url(), pool_pre_ping=True)
 
