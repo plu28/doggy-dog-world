46474066a1ca43b499e53a7a4c83119fa6f63445
David Voronov <davidvoronof@gmail.com>
2024-11-30 12:35:22 -0800

    add: endpoint to retrieve user active game status

--- src/api/games.py
+++ src/api/games.py
@@ -34,6 +34,10 @@ class GameStartResponse(BaseModel):
     game_id: int
     message: str
     player_count: int
+
+class UserStatus(BaseModel):
+    in_lobby: bool
+    is_admin: bool
     
 # queries
 FIND_ACTIVE_GAME_QUERY = """
@@ -87,6 +91,23 @@ FROM players p
 JOIN profiles pr ON p.id = pr.user_id
 LEFT JOIN first_player fp ON true
 WHERE p.game_id = :game_id
+"""
+
+GET_USER_STATUS_QUERY = """
+WITH first_player AS (
+    SELECT p.id, p.game_id 
+    FROM players p 
+    WHERE p.game_id = (SELECT id FROM active_game)
+    ORDER BY p.id 
+    LIMIT 1
+)
+SELECT 
+    (SELECT id FROM active_game) AS game_id,
+    CASE WHEN fp.id = p.id THEN true ELSE false END as is_admin
+FROM players p
+LEFT JOIN first_player fp ON true
+WHERE p.game_id = (SELECT id FROM active_game)
+AND p.id = :user_id
 """
 
 CHECK_IF_USER_HAS_BALANCE_QUERY = """
@@ -442,3 +463,32 @@ async def start_game(
             status_code=400,
             detail=f"Failed to start game: {str(e)}"
         )
+
+# get a users game status
+@router.get("/user_status", response_model=UserStatus)
+async def user_status(user = Depends(get_current_user)):
+    """
+    For the currently active game, returns if the user is in the lobby for that game and also if they are an admin.
+    """
+    try:
+        user_id = user.user.user_metadata['sub']
+        print(user_id)
+        with db.engine.begin() as conn: 
+            status = conn.execute(sqlalchemy.text(GET_USER_STATUS_QUERY), {'user_id': user_id}).fetchone()
+            if not status:
+                return UserStatus(
+                    in_lobby=False,
+                    is_admin=False
+                )
+            else:
+                return UserStatus(
+                    in_lobby=True,
+                    is_admin=status.is_admin
+                )
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(
+            status_code=500,
+            detail=f"Failed to retrieve user status: {str(e)}"
+        )
+
