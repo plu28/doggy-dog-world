5ebdb0caf8a63f2f65cd204b80263260ef3fa4a1
CandyCaneCazoo <58153713+Axelph237@users.noreply.github.com>
2024-12-01 12:49:43 -0800

    Added: async image generation to entrant creation

--- src/api/entrants.py
+++ src/api/entrants.py
@@ -20,17 +20,16 @@ class Entrant(BaseModel):
 
 
 @router.post("/")
-def create_entrant(entrant: Entrant, user = Depends(users.get_current_user)):
+async def create_entrant(entrant: Entrant, user = Depends(users.get_current_user)):
     """
     Given any name and weapon as strings, creates an entrant for the current game.
     Entrant also will have an owner_id set as the requesting user's id.
     """
-    from_thread.run(
-        asyncio.create_task,
+    asyncio.create_task(
         generate_entrant_image(EntrantInfo(name=entrant.name, weapon=entrant.weapon))
     )
 
-    validation_result = asyncio.run(validate_entrant(entrant))
+    validation_result = await validate_entrant(entrant)
     if not validation_result:
         raise HTTPException(
             status_code=400,
